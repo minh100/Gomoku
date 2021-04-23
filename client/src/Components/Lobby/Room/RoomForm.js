4c8a22825e44406f929e47fc21960721d689bd2a
minh100 <minh100@live.unc.edu>
2021-04-22 20:25:45 -0400

    BUG: CREATE GAME FORM REQUIREMENTS

--- client/src/Components/Lobby/Room/RoomForm.js
+++ client/src/Components/Lobby/Room/RoomForm.js
@@ -164,7 +164,7 @@ export const RoomForm = () => {
                         <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
                             <div className="relative w-auto my-6 mx-auto max-w-3xl">
                                 {/*content*/}
-                                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                <form onSubmit={handleCreateGame} className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                                     {/*header*/}
                                     <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
                                         <h3 className="text-3xl font-semibold">
@@ -221,10 +221,7 @@ export const RoomForm = () => {
                                         <div className="flex items-center justify-end p-4 border-t border-solid border-blueGray-200 rounded-b">
                                             <button
                                                 className="bg-green-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                                type="button"
-                                                onClick={() => {
-                                                    handleCreateGame()
-                                                }}
+                                                type="submit"
                                             >
                                                 Create Game
                                             </button>
@@ -240,7 +237,7 @@ export const RoomForm = () => {
                                             </button>
                                         </div>
                                     </div>
-                                </div>
+                                </form>
                             </div>
                         </div>
                     </>
